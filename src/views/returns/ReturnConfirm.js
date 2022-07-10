8e69966c9a47e23e28cff88ece1e3d09f853376b
moses1206 <33392504+moses1206@users.noreply.github.com>
2022-07-10 10:41:17 +0900

    returnConfirm Style Fix

--- src/views/returns/ReturnConfirm.js
+++ src/views/returns/ReturnConfirm.js
@@ -15,47 +15,11 @@ export default function ReturnConfirm({
   return (
     <View style={styles.tableContainer}>
       <ScrollView>
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              width: 340,
-              height: 30,
-              backgroundColor: '#01987A',
-              borderBottomWidth: 0.6,
-            }}
-          >
-            <Text
-              style={{
-                flex: 1,
-                textAlign: 'center',
-                color: '#fff',
-                fontSize: 16,
-              }}
-            >
-              NO
-            </Text>
-            <Text
-              style={{
-                flex: 3,
-                textAlign: 'center',
-                color: '#fff',
-                fontSize: 16,
-              }}
-            >
-              제품명
-            </Text>
-            <Text
-              style={{
-                flex: 1,
-                textAlign: 'center',
-                color: '#fff',
-                fontSize: 16,
-              }}
-            >
-              수량
-            </Text>
+        <View style={styles.lineContainer}>
+          <View style={styles.headerContainer}>
+            <Text style={[styles.headerStyle, { flex: 1 }]}>NO</Text>
+            <Text style={[styles.headerStyle, { flex: 3 }]}>제품명</Text>
+            <Text style={[styles.headerStyle, { flex: 1 }]}>수량</Text>
           </View>
         </View>
 
@@ -65,68 +29,44 @@ export default function ReturnConfirm({
               key={index}
               style={{ justifyContent: 'center', alignItems: 'center' }}
             >
-              <View
-                style={{
-                  flexDirection: 'row',
-                  marginTop: 2,
-                  alignItems: 'center',
-                  width: 340,
-                  borderBottomWidth: 0.3,
-                  height: 30,
-                }}
-              >
-                <Text style={{ flex: 1, textAlign: 'center' }}>
-                  {item.product_no}
-                </Text>
-                <Text style={{ flex: 3, marginLeft: 40 }}>
-                  {item.product_returnName}
-                </Text>
-                <Text style={{ flex: 1, textAlign: 'center' }}>
-                  {item.product_returnCount}
-                </Text>
+              <View style={styles.rowContainer}>
+                <Text style={styles.row}>{item.product_no}</Text>
+                <Text style={styles.rowMiddle}>{item.product_returnName}</Text>
+                <Text style={styles.row}>{item.product_returnCount}</Text>
               </View>
             </View>
           )
         })}
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <View style={styles.totalContainer}>
           <View>
-            <View
-              style={{
-                flexDirection: 'row',
-                marginTop: 20,
-                alignItems: 'center',
-                width: 340,
-                borderBottomWidth: 0.3,
-                height: 30,
-              }}
-            >
+            <View style={[styles.rowContainer, { marginTop: 20 }]}>
               <Text style={{ flex: 3, textAlign: 'center' }}>합계수량 :</Text>
               <Text style={{ flex: 1, marginLeft: 40 }}></Text>
               <Text style={{ flex: 5, textAlign: 'right' }}>
                 {Math.round(returnSumEA)} EA
               </Text>
             </View>
-            <View
-              style={{
-                flexDirection: 'row',
-                marginTop: 2,
-                alignItems: 'center',
-                width: 340,
-                borderBottomWidth: 0.3,
-                height: 30,
-              }}
-            >
+            <View style={styles.rowContainer}>
               <Text style={{ flex: 3, textAlign: 'center' }}>합계금액 :</Text>
               <Text style={{ flex: 1, marginLeft: 40 }}></Text>
               <Text style={{ flex: 5, textAlign: 'right' }}>{TotalSum} 원</Text>
             </View>
           </View>
-          <View style={{ padding: 40 }}>
-            <Text style={{ fontSize: 17 }}> 위의 내역을 등록하시겠습니까?</Text>
-          </View>
         </View>
-
-        <View style={{ positon: 'absolute', left: 0, right: 0, bottom: 0 }}>
+        <View
+          style={{
+            positon: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 0,
+            marginTop: 30,
+          }}
+        >
+          <Text style={{ fontSize: 17, textAlign: 'center' }}>
+            위의 내역을 등록하시겠습니까?
+          </Text>
+        </View>
+        <View style={{ marginTop: 40 }}>
           <Button color='#ff7d0d' title='등 록 하 기' onPress={returnSumbit} />
         </View>
       </ScrollView>
@@ -139,4 +79,28 @@ const styles = StyleSheet.create({
     padding: 10,
     flex: 1,
   },
+  lineContainer: { justifyContent: 'center', alignItems: 'center' },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: 340,
+    height: 30,
+    backgroundColor: '#01987A',
+    borderBottomWidth: 0.6,
+  },
+  headerStyle: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 16,
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    marginTop: 2,
+    alignItems: 'center',
+    width: 340,
+    borderBottomWidth: 0.3,
+    height: 30,
+  },
+  row: { flex: 1, textAlign: 'center' },
+  rowMiddle: { flex: 3, marginLeft: 40 },
 })
