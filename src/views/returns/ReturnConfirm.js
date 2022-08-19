ac848b672c10738bdbdc16dfafe912a0697ad3dd
unknown <mose23@naver.com>
2022-08-19 22:13:52 +0900

    trying returnBag Process

--- src/views/returns/ReturnConfirm.js
+++ src/views/returns/ReturnConfirm.js
@@ -6,20 +6,23 @@ import {
   StyleSheet,
   TextInput,
   Alert,
-} from 'react-native'
-import React, { useState } from 'react'
-import returnBagSlice from '../../redux/slices/returnbag'
-import { useDispatch, useSelector } from 'react-redux'
-import { gunnyNumber, yearMonth } from '../../datas/ReturnData'
-import { Picker } from 'react-native-woodpicker'
+} from "react-native";
+import React, { useState } from "react";
+import returnBagSlice from "../../redux/slices/returnbag";
+import { useDispatch, useSelector } from "react-redux";
+import { gunnyNumber, yearMonth } from "../../datas/ReturnData";
+import { Picker } from "react-native-woodpicker";
 import {
   VerticalDiv,
   HorizontalDiv,
   ProtmotionCreateContainer,
-} from '../../styles/PromotionStyle'
-import CategoryOfGunny from '../../components/CategoryOfReturnGunny'
-import CategoryOfYearMohth from '../../components/CategoryOfReturnYearMonth'
-import { useRetrunBagList, useReturnBagCreation } from '../../hooks/returnHooks'
+} from "../../styles/PromotionStyle";
+import CategoryOfGunny from "../../components/CategoryOfReturnGunny";
+import CategoryOfYearMohth from "../../components/CategoryOfReturnYearMonth";
+import {
+  useRetrunBagList,
+  useReturnBagCreation,
+} from "../../hooks/returnHooks";
 
 export default function ReturnConfirm({
   navigation,
@@ -28,49 +31,51 @@ export default function ReturnConfirm({
   filteredReturnList,
 }) {
   const [returnBagNumber, setReturnBagNumber] = useState({
-    label: '1번',
+    label: "1번",
     value: 1,
-  })
+  });
   const [returnDate, setReturnDate] = useState({
-    label: '22년 1월',
+    label: "22년 1월",
     value: 1,
-  })
+  });
 
   // Redux
-  const dispatch = useDispatch()
-  const token = useSelector((state) => state.user.token)
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.user.token);
 
-  const Sum = Math.round(returnSumPrice)
-  const TotalSum = Sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  const Sum = Math.round(returnSumPrice);
+  const TotalSum = Sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   const returnSumbit = async () => {
-    const returnObj = {
-      gunnyNumber: returnBagNumber,
-      YearMonth: returnDate,
-      totalQty: returnSumEA,
-      totalValue: returnSumPrice,
-      gunnySack: filteredReturnList,
-    }
-
     try {
-      const data = await useReturnBagCreation(token, returnObj)
+      const returnObj = {
+        gunnyNumber: returnBagNumber,
+        yearMonth: returnDate,
+        totalQty: returnSumEA,
+        totalValue: returnSumPrice,
+        gunnySack: JSON.stringify(filteredReturnList),
+      };
+
+      const data = await useReturnBagCreation(token, returnObj);
+
+      console.log("reponseData", data);
 
       if (data) {
-        const response = await useRetrunBagList(token)
+        const response = await useRetrunBagList(token);
         if (response) {
           dispatch(
             returnBagSlice.actions.setReturnBag({
               array: [...response],
             })
-          )
+          );
         }
       }
-      Alert.alert('반품이 등록되었습니다!!')
-      navigation.navigate('반품 리스트')
+      Alert.alert("반품이 등록되었습니다!!");
+      navigation.navigate("반품 리스트");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   return (
     <View style={styles.tableContainer}>
@@ -103,7 +108,7 @@ export default function ReturnConfirm({
           return (
             <View
               key={index}
-              style={{ justifyContent: 'center', alignItems: 'center' }}
+              style={{ justifyContent: "center", alignItems: "center" }}
             >
               <View style={styles.rowContainer}>
                 <Text style={styles.row}>{item.product_no}</Text>
@@ -111,43 +116,43 @@ export default function ReturnConfirm({
                 <Text style={styles.row}>{item.product_returnCount}</Text>
               </View>
             </View>
-          )
+          );
         })}
         <View style={styles.totalContainer}>
           <View>
             <View style={[styles.rowContainer, { marginTop: 20 }]}>
-              <Text style={{ flex: 3, textAlign: 'center' }}>합계수량 :</Text>
+              <Text style={{ flex: 3, textAlign: "center" }}>합계수량 :</Text>
               <Text style={{ flex: 1, marginLeft: 40 }}></Text>
-              <Text style={{ flex: 5, textAlign: 'right' }}>
+              <Text style={{ flex: 5, textAlign: "right" }}>
                 {Math.round(returnSumEA)} EA
               </Text>
             </View>
             <View style={styles.rowContainer}>
-              <Text style={{ flex: 3, textAlign: 'center' }}>합계금액 :</Text>
+              <Text style={{ flex: 3, textAlign: "center" }}>합계금액 :</Text>
               <Text style={{ flex: 1, marginLeft: 40 }}></Text>
-              <Text style={{ flex: 5, textAlign: 'right' }}>{TotalSum} 원</Text>
+              <Text style={{ flex: 5, textAlign: "right" }}>{TotalSum} 원</Text>
             </View>
           </View>
         </View>
         <View
           style={{
-            positon: 'absolute',
+            positon: "absolute",
             left: 0,
             right: 0,
             bottom: 0,
             marginTop: 30,
           }}
         >
-          <Text style={{ fontSize: 17, textAlign: 'center' }}>
+          <Text style={{ fontSize: 17, textAlign: "center" }}>
             위의 내역을 등록하시겠습니까?
           </Text>
         </View>
         <View style={{ marginTop: 40 }}>
-          <Button color='#ff7d0d' title='등 록 하 기' onPress={returnSumbit} />
+          <Button color="#ff7d0d" title="등 록 하 기" onPress={returnSumbit} />
         </View>
       </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -156,32 +161,32 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   lineContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 15,
   },
   headerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     width: 340,
     height: 30,
-    backgroundColor: '#01987A',
+    backgroundColor: "#01987A",
     borderBottomWidth: 0.6,
   },
   headerStyle: {
-    textAlign: 'center',
-    color: '#fff',
+    textAlign: "center",
+    color: "#fff",
     fontSize: 16,
   },
   rowContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 2,
-    alignItems: 'center',
+    alignItems: "center",
     width: 340,
     borderBottomWidth: 0.3,
     height: 30,
   },
-  row: { flex: 1, textAlign: 'center' },
+  row: { flex: 1, textAlign: "center" },
   rowMiddle: { flex: 3, marginLeft: 40 },
   input: {
     width: 100,
@@ -189,17 +194,17 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginLeft: 10,
     fontSize: 15,
-    color: '#006aff',
+    color: "#006aff",
     borderWidth: 2,
     borderRadius: 10,
     zIndex: 10,
-    textAlign: 'center',
+    textAlign: "center",
   },
   animatedStyle: {
     top: 25,
     left: 15,
-    position: 'absolute',
+    position: "absolute",
     borderRadius: 90,
     zIndex: 1,
   },
-})
+});
