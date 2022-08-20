95e30d7bf45c52a32354b86f9717adc395de867e
unknown <mose23@naver.comm>
2022-08-20 14:10:52 +0900

    Bug Fix

--- src/hooks/returnHooks.js
+++ src/hooks/returnHooks.js
@@ -1,18 +1,20 @@
-import axios from "axios";
-import { basicApiUrl } from "../secrets/urlSetting";
+import axios from 'axios'
+import { basicApiUrl } from '../secrets/urlSetting'
 
 export const useReturnBagCreation = async (token, returnObj) => {
+  console.log('ReturnObj', returnObj)
+
   const { data } = await axios.post(`${basicApiUrl}/returnbag`, returnObj, {
     headers: { authorization: `Bearer ${token}` },
-  });
+  })
 
-  return data;
-};
+  return data
+}
 
 export const useRetrunBagList = async (token) => {
   const { data } = await axios.get(`${basicApiUrl}/returnbag`, {
     headers: { authorization: `Bearer ${token}` },
-  });
+  })
 
-  return data.myOrders;
-};
+  return data.myReturnBag
+}
